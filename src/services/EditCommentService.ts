e85bb2298dd2538c4318dd97eeab1cf7fc94ba32
Fernando Silva <fernandowonder123@gmail.com>
2023-12-24 02:04:59 +0100

    Add feat edit commment

---
+++ src/services/EditCommentService.ts
@@ -0,0 +1,18 @@
+import { hash } from "bcryptjs";
+import { IUserRepository } from "../repositories/UserRepository";
+import { IUser } from "../entitites/User";
+import { ResourceDontExist } from "../errors/ResourceDontExists";
+import { IPostRepository } from "../repositories/PostRepository";
+import { IPost } from "../entitites/Post";
+import { ICommentRepository } from "../repositories/CommentRepository";
+
+interface IComment{
+    comment?: string,
+}
+export class EditCommentstService {
+    constructor(private commentRespository: ICommentRepository, private postRepository: IPostRepository,private userRepository: IUserRepository) {}
+    async execute(data :IComment,id:string){
+        const comment = await this.commentRespository.update(data,id)
+        return comment
+     }
+}
